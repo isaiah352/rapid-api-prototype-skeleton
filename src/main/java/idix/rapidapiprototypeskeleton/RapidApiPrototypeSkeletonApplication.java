63b61de4944a34ea0bc8662988c227dfd5cf1c79
Isaiah Dix <dix.isaiah@gmail.com>
2025-02-24 16:42:50 -0500

    initial commit.

---
+++ src/main/java/idix/rapidapiprototypeskeleton/RapidApiPrototypeSkeletonApplication.java
@@ -0,0 +1,13 @@
+package idix.rapidapiprototypeskeleton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RapidApiPrototypeSkeletonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RapidApiPrototypeSkeletonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/idix/rapidapiprototypeskeleton/RapidApiPrototypeSkeletonApplicationTests.java
@@ -0,0 +1,13 @@
+package idix.rapidapiprototypeskeleton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RapidApiPrototypeSkeletonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
