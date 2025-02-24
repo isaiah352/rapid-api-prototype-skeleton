2c36d843a3d700c87dcfcea977b16efc8c1bed2d
Isaiah Dix <dix.isaiah@gmail.com>
2025-02-24 17:34:31 -0500

    adding h2 scripts

---
+++ README.md
@@ -0,0 +1,4 @@
+**H2 in memory database:**
+To connect to H2 while running the application locally, use the URL below.
+localhost:8080/h2-console
+Once there, use jdbc:h2:mem:testdb as the datasource to login.
---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO CUSTOMERS (first_name, last_name, description)
+VALUES ( 'Dan', 'Vega', 'Electronics customer' ),
+       ( 'Sarah', 'Smith', 'Sporting goods customer' ),
+       ( 'Dan', 'Brown', 'Grocery customer' );
